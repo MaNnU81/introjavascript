a7419b6c23eff5b9009f04692dddeec1a28055ff
MaNnu81 <mannuspamm@gmail.com>
2025-02-17 10:58:14 +0100

    js prima volta

---
+++ main.js
@@ -0,0 +1,58 @@
+console.log("hello world");
+
+console.log("ciao mondo");
+
+// typeo
+
+console.log(typeof 5);
+console.log(typeof "hello world");
+console.log(typeof true);
+
+//numbers
+
+5 //integer
+7.68 //float
+//310000 esponenziali
+3.1e5
+
+Infinity //numero piu grande possibile
+-Infinity //numero piu piccolo possibile
+NaN //Not a Number...ma è un numero
+console.log(typeof NaN)  //verifica che sia veramente numero
+console.log(0/0)  //prova per far uscire un NaN
+console.log("somma 5+7", 5+7, "ciao");
+console.log("sottrazione 5-7", 5-7);
+console.log("prodotto 5*7", 5*7);
+console.log("divsione 5/7", 5/7);
+console.log("potenza 2**5", 5**7);
+
+//MODULO
+console.log("modulo 5%2", 5 % 2 );
+console.log("modulo 12%5", 12 % 5 );
+
+//tutte le operazioni possibile sono queste, per altre bisogna usare MATH
+
+console.log("radice quadrata 9", Math.sqrt(9));
+console.log("arrotondamento 9.6", Math.round(9.6)); //math.round come matematica, math.floor verso il basso math.ceil verso l'alto
+
+//stringhe testo, anche se sono numeri ma scritto risultano testo
+console.log("234", 234);
+console.log("io sono una stringa");
+console.log('io sono una stringa');
+console.log('io sono una "stringa"');
+console.log("\"l'apostrofo\" è importante");
+console.log("\"l'apostrofo\" è importante \n non ne posso fare a meno");
+
+console.log(`"l'apostrofo" è importante
+    non ne posso fare  a meno`);   //alt+96 va a capo se vado a capo senza comando \n e al suo interno posso inserire del codice, non esiste in italiano e non rompe come le "" o ''
+    
+console.log(` la somma di 5 e 7 è ${7+5}`);  //esempio
+
+console.log('ciao a tutti' + ' sono Mannu');
+
+
+
+
+
+
+
