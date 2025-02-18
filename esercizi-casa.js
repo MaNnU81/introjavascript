2c590966ece9a8d67bc3ce77d7affca6e3e57261
MaNnu81 <mannuspamm@gmail.com>
2025-02-18 18:46:11 +0100

    primi esercizi

---
+++ esercizi-casa.js
@@ -0,0 +1,33 @@
+
+// console.log("1-Scalinata in un ciclo");
+
+// let startingString = "";
+// let finalString = "";   //accumula startString fino a fine ciclo
+// for (let i = 0; i < 7; i++) {
+//    startingString += "#";
+//    finalString += startingString + "\n" //accumula startString fino a fine ciclo e va a capo
+// }
+// console.log(finalString)
+
+
+
+// // console.log("2-Scalinata in un ciclo capovolta");
+
+// let startingString = "";
+// let finalString = "";   //accumula startString fino a fine ciclo
+// for (let i = 7; i > 0; i--) {
+//    startingString -= "#";
+//    finalString += startingString + "\n" //accumula startString fino a fine ciclo e va a capo
+// }
+// console.log(finalString) 
+
+// console.log("4- usando un ciclo logga i numeri da 100 a -100")
+
+// let startString = ""
+// let finalString = ""
+// for (let startString = 100; startString >= -100; startString--) {
+//     finalString += startString + "\n"
+// }
+// console.log(finalString);
+
+
