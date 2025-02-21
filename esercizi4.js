08df6455e317c80eb9afd83a34e08e3904f1f0fe
MaNnu81 <mannuspamm@gmail.com>
2025-02-21 13:10:54 +0100

    ef

--- esercizi4.js
+++ esercizi4.js
@@ -1,4 +1,69 @@
 //1) creare un oggetto studente per ogni partecipante al corso 
+
+
+const lorenzo = {
+    name: 'lorenzo',
+    surname: 'puppo',
+    yob: 1995,
+    nationality: 'italy',
+    gender: 'm',
+  }
+  
+  const Jan = {
+    name: 'jan',
+    surname: 'stigliani',
+    yob: 2000,
+    nationality: 'italy',
+    gender: 'm'
+  }
+  
+  const giovanni = {
+    name: 'giovanni',
+    surname: 'sussarellu',
+    yob: 1981,
+    nationality: 'italy',
+    gender: 'm'
+  }
+  
+  const sara =  {
+    name: 'sara',
+    surname: 'd prà',
+    yob: 1989,
+    nationality: 'italy',
+    gender: 'fluid'
+  }
+  
+  const jeremias = {
+    name: 'jeremias',
+    surname: 'cedeno',
+    yob:2003,
+    nationality: 'ecuador',
+    gender: 'm'
+  }
+  
+  const laura = {
+    name: 'laura',
+    surname: 'mazza',
+    yob: 1884,
+    nationality: 'italy',
+    gender: 'f'
+  }
+  
+  const eusebio = {
+    name: 'eusebio',
+    surname: 'veizi',
+    yob: 1993,
+    nationality: 'albanese',
+    gender: 'peanut'
+  }
+  
+  const hugo = {
+    name: 'hugo',
+    surname: 'martinez',
+    yob: 1994,
+    nationality: 'elSalvador',
+    gender: 'f'
+  }
 // l'oggetto dovà avere le seguenti keys : name, surname, gender, marks, yob, region, nationality
 
 
