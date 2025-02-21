8f449e711033c74cfb742c2fc94a455ab66dd611
MaNnu81 <mannuspamm@gmail.com>
2025-02-21 13:28:30 +0100

    g

--- esercizi4.js
+++ esercizi4.js
@@ -1,9 +1,10 @@
 //1) creare un oggetto studente per ogni partecipante al corso 
-
+// l'oggetto dovà avere le seguenti keys : name, surname, gender, marks, yob, region, nationality
 
 const lorenzo = {
     name: 'lorenzo',
     surname: 'puppo',
+    marks: [7, 8 ,4],
     yob: 1995,
     nationality: 'italy',
     gender: 'm',
@@ -12,6 +13,7 @@ const lorenzo = {
   const Jan = {
     name: 'jan',
     surname: 'stigliani',
+    marks:  [7, 9 ,9],
     yob: 2000,
     nationality: 'italy',
     gender: 'm'
@@ -20,6 +22,7 @@ const lorenzo = {
   const giovanni = {
     name: 'giovanni',
     surname: 'sussarellu',
+    marks:  [5, 8 ,3],
     yob: 1981,
     nationality: 'italy',
     gender: 'm'
@@ -27,7 +30,8 @@ const lorenzo = {
   
   const sara =  {
     name: 'sara',
-    surname: 'd prà',
+    surname: 'de prà',
+    marks:  [9, 4 ,7],
     yob: 1989,
     nationality: 'italy',
     gender: 'fluid'
@@ -36,6 +40,7 @@ const lorenzo = {
   const jeremias = {
     name: 'jeremias',
     surname: 'cedeno',
+    marks:  [7, 9 ,9],
     yob:2003,
     nationality: 'ecuador',
     gender: 'm'
@@ -44,7 +49,8 @@ const lorenzo = {
   const laura = {
     name: 'laura',
     surname: 'mazza',
-    yob: 1884,
+    marks:  [3, 8 ,9],
+    yob: 1984,
     nationality: 'italy',
     gender: 'f'
   }
@@ -52,6 +58,7 @@ const lorenzo = {
   const eusebio = {
     name: 'eusebio',
     surname: 'veizi',
+    marks:  [8, 8 ,8],
     yob: 1993,
     nationality: 'albanese',
     gender: 'peanut'
@@ -60,16 +67,44 @@ const lorenzo = {
   const hugo = {
     name: 'hugo',
     surname: 'martinez',
+    marks:  [5, 8 ,8],
     yob: 1994,
     nationality: 'elSalvador',
     gender: 'f'
   }
-// l'oggetto dovà avere le seguenti keys : name, surname, gender, marks, yob, region, nationality
 
 
-//2) scrivete una funzione che restituisca l'eta anagarfica degli studenti
+const arraystudents = [lorenzo, jeremias, giovanni, Jan, laura, sara, hugo, eusebio];
+
+
+console.log(arraystudents);
+
+
+//2) scrivete una funzione che restituisca l'eta media degli studenti
+
+function meanAge(array) {
+    let media = 0;
+
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+
+    
+}
+
+    return media;
+}
+
+
+function getAge(yob, yot) {
+
+
+    return yot - yob
+}
 //3 scrivete una funzione che restituisca im array di stringhe ed ogni stringa dovrà 
 //  mettete gli oggetti dentro un arrayavere questo formato "nome/cognome"
 //4) scrivete una funzione che restituisca la somma dei maschi e delle femmine
 //5) scrivete una funzione che restituisca la media delle medie degli studenti
 //6) scrivete una funzione che restituisca un ogetto cosi strutturato
+
+
+//7) https://jessbayer.com/
