84b1568a5eb7487e5ff952fa04321fb80cf83606
MaNnu81 <mannuspamm@gmail.com>
2025-03-03 23:57:06 +0100

    super

--- esercizi6.js
+++ esercizi6.js
@@ -160,7 +160,7 @@ bestOfTheBest() {
     return bestStudent ? `${bestStudent.name} ${bestStudent.surname} con media ${highestMean}` : "Nessun vincitore";
 
 
-
+ 
 }
 
 bestTeacher() {
@@ -177,7 +177,7 @@ bestTeacher() {
             bestTeacher = teacher;
         }
     }
-    return bestTeacher ? `${bestTeacher.name} ${bestTeacher.surname} con media ${bestMean}` : "Nessun insegnante trovato";
+    return bestTeacher ? `il miglior insegnante è ${bestTeacher.name} ${bestTeacher.surname} ` : "Nessun insegnante trovato";
 }
 
 
@@ -209,8 +209,8 @@ const principal1 = new Principal(`seymor`, `skinner`, `1969`, `USA`, `M`, `sprin
 // console.log(student1.toString());
 // console.log(teacher1.bestStudent());
 // console.log(teacher1.StudentsMean());
-console.log(principal1.bestOfTheBest());
-console.log(principal1.bestTeacher());
+// console.log(principal1.bestOfTheBest());
+// console.log(principal1.bestTeacher());
 
 
 
@@ -223,3 +223,134 @@ console.log(principal1.bestTeacher());
 // -un array di teachers
 //-il nome della scuola
 //4)aggiungere al principal due funzioni : bestTeacher (la media delle medie migliori studentsMean) e bestStudent (migliore studente tra tutti)
+
+
+///////////////////////////
+
+
+class ContoBancario {
+    constructor(name, surname, yob, nationality, gender, saldo) {
+        this.name = name;
+        this.surname = surname;
+        this.yob = yob;
+        this.nationality = nationality;
+        this.gender = gender;
+        this.saldo = Number(saldo)
+        if (isNaN(this.saldo)) {
+            this.saldo = 0;
+        }
+    }
+    getAge() {
+        const now = new Date();   //data di oggi
+        const year = now.getFullYear();
+        const age = year - this.yob;
+        return age;
+    }
+    toString() {
+        const humanStr = `nome : ${this.name}
+cognome : ${this.surname}
+eta : ${this.getAge()}
+genere : ${this.gender}
+nazionalità : ${this.nationality}
+saldo : ${this.saldo}`
+        return humanStr;
+    }
+
+    preleva(importo){
+        if (typeof importo === "number" && this.saldo - importo > 0) {
+            this.saldo -= importo;
+            
+        }else{
+            console.log("Somma non disponibile");
+            
+        }
+        return this.saldo;
+    }
+    
+    versamento(importo){
+        if (typeof importo === "number") {
+            return this.saldo += importo;
+            
+        }else{
+            console.log("caratteri errati");
+            
+        }
+        
+    }
+}
+
+class ContoCorrente extends ContoBancario{
+    constructor(name, surname, yob, nationality, gender, saldo, limiteSpesa){
+        super(name, surname, yob, nationality, gender, saldo);
+        this.limiteSpesa = Number(limiteSpesa)
+        if (isNaN(this.limiteSpesa)) {
+            this.limiteSpesa = 0;
+        }
+        
+    }
+
+    paga(importo){
+       if (importo <= this.limiteSpesa && importo <= this.saldo) {
+        this.saldo -= importo + (importo * -0.02);
+       }else{
+            console.log("Somma non disponibile");
+            
+        }
+        return this.saldo;
+    }
+}
+
+class ContoRisparmio extends ContoBancario {
+    constructor(name, surname, yob, nationality, gender, saldo, sogliaBonus){
+        super(name, surname, yob, nationality, gender, saldo);
+        this.sogliaBonus = Number(sogliaBonus)
+        if (isNaN(this.sogliaBonus)) {
+            this.sogliaBonus = 0;
+        }
+    }
+
+    applicaBonus(){
+        if (this.saldo > this.sogliaBonus) {
+            return this.saldo *= 1.02;
+            
+        };
+        
+    }
+}
+
+const correntista1 = new ContoBancario(`laura`, `mazza`, `1984`, `it`, `f`, `5043`);
+const correntista2 = new ContoBancario(`eusebio`, `veizi`, `1993`, `al`, `m`, `12543`);
+const correntista3 = new ContoBancario(`lorenzo`, `puppo`, `1993`, `it`, `m`, `53213`);
+const correntista5 = new ContoBancario(`hugo`, `martinez`, `1994`, `sv`, `m`, `3425167`);
+const correntista6 = new ContoBancario(`jeremias`, `cedeno`, `2003`, `ec`, `m`, `25000`);
+const correntista7 = new ContoBancario(`sara`, `de prà`, `1989`, `it`, `f`, `324325`);
+const correntista8 = new ContoBancario(`giovanni`, `sussarellu`, `1981`, `it`, `m`, `3245`);
+const correntista9 = new ContoBancario(`jan`, `stigliani`, `2000`, `it`, `m`, `1232`);
+const correntista10 = new ContoBancario(`edna`, `caprapall`, `1972`, `USA`, `f`, `5843`);
+const correntista11 = new ContoBancario(`elizabeth`, `hoover`, `1975`, `USA`, `f`, `750`);
+
+
+const ContoCorrente1 = new ContoCorrente(`laura`, `mazza`, `1984`, `it`, `f`, `5043`, `2000`);
+const ContoCorrente2 = new ContoCorrente(`eusebio`, `veizi`, `1993`, `al`, `m`, `12543`, `2000`);
+const ContoCorrente3 = new ContoCorrente(`lorenzo`, `puppo`, `1993`, `it`, `m`, `53213`, `2000`);
+const ContoCorrente4 = new ContoCorrente(`hugo`, `martinez`, `1994`, `sv`, `m`, `3425167`, `2000`);
+const ContoCorrente5 = new ContoCorrente(`jeremias`, `cedeno`, `2003`, `ec`, `m`, `25000`, `5000`);
+const ContoCorrente6 = new ContoCorrente(`sara`, `de prà`, `1989`, `it`, `f`, `324325`, `2000`);
+const ContoCorrente7 = new ContoCorrente(`giovanni`, `sussarellu`, `1981`, `it`, `m`, `3245`, `2000`);
+const ContoCorrente8 = new ContoCorrente(`jan`, `stigliani`, `2000`, `it`, `m`, `1232`, `2000`);
+const ContoCorrente9 = new ContoCorrente(`edna`, `caprapall`, `1972`, `USA`, `f`, `5843`, `2000`);
+const ContoCorrente10 = new ContoCorrente(`elizabeth`, `hoover`, `1975`, `USA`, `f`, `750`, `2000`);
+
+const ContoRisparmio1 = new ContoRisparmio(`sara`, `de prà`, `1989`, `it`, `f`, `324325`, `2000`);
+const ContoRisparmio2 = new ContoRisparmio(`giovanni`, `sussarellu`, `1981`, `it`, `m`, `10000`, `2000`);
+const ContoRisparmio3 = new ContoRisparmio(`jan`, `stigliani`, `2000`, `it`, `m`, `1232`, `2000`);
+const ContoRisparmio4 = new ContoRisparmio(`edna`, `caprapall`, `1972`, `USA`, `f`, `5843`, `2000`);
+const ContoRisparmio5 = new ContoRisparmio(`elizabeth`, `hoover`, `1975`, `USA`, `f`, `750`, `2000`);
+
+console.log(correntista10.toString());
+console.log('Il nuovo saldo è di ' + correntista10.preleva(5000));
+console.log('Il nuovo saldo è di ' + correntista10.versamento(5000));
+console.log(ContoCorrente2.toString());
+console.log('Il nuovo saldo è di ' + ContoCorrente5.paga(5000));
+console.log('Abbiamo aggiunto il tuo BonusSaldo ' + ContoRisparmio2.applicaBonus());
+
