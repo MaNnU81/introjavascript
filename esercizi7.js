03b00d3e1d0cb2de4750ebf2a6db596514b3c8d3
MaNnu81 <mannuspamm@gmail.com>
2025-03-04 13:26:27 +0100

    Implement book and user management classes with borrowing functionality

--- esercizi7.js
+++ esercizi7.js
@@ -34,3 +34,97 @@
 //         Aggiungere e rimuovere utenti dalla biblioteca.
 //         Gestire il prestito e la restituzione dei libri agli utenti.
 //         Visualizzare l'elenco dei libri disponibili e degli utenti registrati.
+
+class Book {
+    
+    constructor(title, author, isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+
+    // get isbn(){
+    //     return `isbn + ${this.isbn}`;
+    // }
+    // set isbn(value){
+    //     this.isbn = value;
+    // }
+
+    toString(){
+        const bookStr = `
+        titolo : ${this.title}
+        autore : ${this.author}`;
+        return bookStr;
+    }
+
+}
+
+const book1 = new Book(`il signore degli anelli`, `J.R.R. Tolkien`, 80054278);
+
+console.log(book1.isbn);
+
+class PhysicalBook extends Book {
+    constructor(title, author, isbn, shelfLocation) {
+        super(title, author, isbn);
+        this.shelfLocation = shelfLocation;
+    }
+    toString(){
+        const bookphyStr = `
+        Physical Version
+         ${super.toString()}
+        shelfLocation : ${this.shelfLocation}`;
+        return bookphyStr;
+    }
+
+}
+const bookPhy1 = new PhysicalBook(`il signore degli anelli`, `J.R.R. Tolkien`, 80054278, `a-15`);
+console.log(bookPhy1.toString());
+
+class EBook extends Book {
+    constructor(title, author, isbn, fileFormat) {
+        super(title, author, isbn);
+        this.fileFormat = fileFormat;
+    }
+    toString(){
+        const eBookStr = `
+        EBook version 
+         ${super.toString()}
+        fileFormat : ${this.fileFormat}`;
+        return eBookStr;
+    }
+
+}
+const eBook1 = new EBook(`il signore degli anelli`, `J.R.R. Tolkien`, 80054278, `.mobi`);
+console.log(eBook1.toString());
+
+class User {
+    static maxBorrowLimit = 3;
+    constructor(name, id, borrowedBooks = []) {
+        this.name = name;
+        this.id = id;
+        this.borrowedBooks = borrowedBooks;
+    }
+
+    borrowBook(book){
+        if(this.borrowedBooks.length >= User.maxBorrowLimit){
+            console.error('hai superato il limite di libri che puoi prendere in prestito!!!!!!!!!')
+            return 
+        }
+        this.borrowedBooks.push(book);
+        console.log(`hai preso in prestito ${book.title}`);
+    }
+    returnBook(book){
+        if(this.borrowedBooks.length > 0){
+            console.error('che cazzo vuoi, non hai libri da restituire, mi fai perdere tempo')
+            return 
+        }
+        if(this.borrowedBooks.includes(book) ){
+        this.borrowedBooks.splice(book);
+        console.log(`hai restituito il libro finalmente ${book.title}`);
+    } else {
+        console.error('che cazzo vuoi, mi fai perdere tempo, sto libro non e nostro')
+    }
+    }
+}
+const user1 = new User(`Patrik`, `T-1520` );
+console.log(user1.borrowBook(bookPhy1));
